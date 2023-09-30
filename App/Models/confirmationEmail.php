34dd34c6601caa7604f02175c76432e794df4835
yrllanbrandao <yrllanbrandao@outlook.com>
2023-09-30 00:00:31 -0300

    feat(confirmationEmail): confirmation email template

---
+++ App/Models/confirmationEmail.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Ativação de formulário</title>
+</head>
+<body style="font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0;">
+
+<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#0077b6">
+    <tr>
+        <td align="center" style="padding: 40px 0;">
+            <h1 style="color: #ffffff; font-size: 36px; margin: 0;">Confirmação de Cadastro</h1>
+        </td>
+    </tr>
+</table>
+
+<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#ffffff">
+    <tr>
+        <td align="center" style="padding: 40px;">
+            <p style="color: #333333; font-size: 18px;">Olá!</p>
+            <p style="color: #333333; font-size: 16px;">Obrigado por utilizar o <strong>form Submitter</strong>.</p>
+            <p style="color: #333333; font-size: 16px;">Para ativar seu formulário, clique no botão abaixo:</p>
+            
+
+            <a href="[Link de Confirmação]" style="background-color: #0077b6; color: #ffffff; text-decoration: none; font-weight: bold; padding: 10px 20px; border-radius: 5px; display: inline-block;">Confirmar Cadastro</a>
+            
+            <p style="color: #333333; font-size: 16px;">Se você não solicitou a ativação de formulários em nosso serviço, ignore este e-mail.</p>
+        </td>
+    </tr>
+</table>
+
+<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#f4f4f4">
+    <tr>
+        <td align="center" style="padding: 40px;">
+            <p style="color: #333333; font-size: 12px;">Este e-mail foi enviado por [Nome da Sua Empresa].</p>
+        </td>
+    </tr>
+</table>
+
+</body>
+</html>
